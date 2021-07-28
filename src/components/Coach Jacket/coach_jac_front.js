83287515c005d8bd5afd77774696a9fc8e5ddd8b
jamshaidiqbal <jamshaid1824@gmail.com>
2021-07-28 14:04:30 +0500

    color issues

--- src/components/Coach Jacket/coach_jac_front.js
+++ src/components/Coach Jacket/coach_jac_front.js
@@ -124,10 +124,12 @@ function SamLocalEditorCoachJacFront(props) {
     const loadObject = (obj) => {
         fabric.Image.fromURL(obj.src, function (img) {
             // img.id = id;
-            img.filters = [new fabric.Image.filters.HueRotation()];
             if(obj.color){
-                var hue=hexatoHSL(obj.color.hex)
-                img.filters[0].rotation = hue
+                img.filters.push(new fabric.Image.filters.BlendColor({
+                    color: obj.color.hex,
+                    mode: 'tint',
+                    opacity: 0
+                }))
             }
 
             img.applyFilters()
@@ -950,39 +952,6 @@ function SamLocalEditorCoachJacFront(props) {
 
     }
 
-
-    const hexatoHSL=(hex)=> {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        var r = parseInt(result[1], 16);
-        var g = parseInt(result[2], 16);
-        var b = parseInt(result[3], 16);
-        var max = Math.max(r, g, b), min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
-        if (max == min) {
-            h = s = 0; // achromatic
-        } else {
-            var d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch (max) {
-                case r:
-                    h = (g - b) / d + (g < b ? 6 : 0);
-                    break;
-                case g:
-                    h = (b - r) / d + 2;
-                    break;
-                case b:
-                    h = (r - g) / d + 4;
-                    break;
-            }
-            h /= 6;
-        }
-        s = s * 100;
-        s = Math.round(s);
-        l = l * 100;
-        l = Math.round(l);
-        var colorInHSL =  h;
-        return colorInHSL
-    }
 
     const addShape = (e) => {
         let type = e.target.name;
